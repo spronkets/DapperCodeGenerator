4575d8b5510e27b77076bf3715f36886f5a45a47
clebron949 <christopher.lebron1@gmail.com>
2025-08-06 11:19:34 -0400

    add initial blazor project

---
+++ DapperCodeGenerator.WebBlazor/Components/Shared/CopyButton.razor.js
@@ -0,0 +1,23 @@
+﻿export function copyElementTextById(elementId) {
+    const el = document.getElementById(elementId);
+    if (document.body.createTextRange) {
+        const textRange = document.body.createTextRange();
+        textRange.moveToElementText(el);
+        textRange.select();
+        textRange.execCommand('Copy');
+    }
+    else if (window.getSelection && document.createRange) {
+        const range = document.createRange();
+        range.selectNodeContents(el);
+        const sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+        try {
+            const successful = document.execCommand('copy');
+            const msg = successful ? 'successful' : 'unsuccessful';
+            console.log(`Copy ${msg}`);
+        } catch (err) {
+            console.log('Copy unsuccessful');
+        }
+    }
+}
---
+++ DapperCodeGenerator.WebBlazor/Extensions/MarkdownExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Markdig;
+
+namespace DapperCodeGenerator.WebBlazor.Extensions;
+
+public static class MarkdownExtensions
+{
+    public static string ToHtml(this string markdown)
+    {
+        return Markdown.ToHtml(markdown ?? "");
+    }
+}
